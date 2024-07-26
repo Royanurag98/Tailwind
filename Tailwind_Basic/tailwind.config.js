fcf307b5b17c387becebdb8e5967d1930781e840
Royanurag98 <akroymarwa98@gmail.com>
2024-07-26 22:50:35 +0530

    Tailwind css Code

---
+++ Tailwind_Basic/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      exs:'450px',
+      sm: '680px',
+      md: '960px',
+      lg: '1024px',
+      xl: '1240px',
+     
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
+
---
+++ Tailwind_grid/tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+      'ex': '2652px',
+    
+    },
+    extend: {
+      screens: {
+        'ex': {'min':'1536px' , 'max':'2652px' },
+        // => @media (min-width: 992px) { ... }
+      },
+    },
+  },
+  plugins: [],
+}
+
